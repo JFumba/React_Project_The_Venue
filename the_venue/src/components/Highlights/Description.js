f8254923b6815188045abc6dbd4042b35762e39e
Johnson <jessefumba@gmail.com>
2018-11-16 14:09:40 +0100

    highlights

---
+++ the_venue/src/components/Highlights/Description.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus lacus vitae risus auctor, eu varius velit mattis. Aliquam mollis, purus id tincidunt vehicula, tortor mi varius tortor, non iaculis dui massa at tellus. Sed diam erat, finibus fringilla molestie at, condimentum eget magna. Phasellus eu lectus ac dolor aliquam dignissim. Pellentesque eget pharetra odio. Ut commodo neque cursus odio imperdiet, sed convallis lorem maximus. Mauris quis felis mi. Vivamus at tincidunt nisl. Duis molestie volutpat ex, eu maximus ligula mollis eget. Sed a ullamcorper purus. Pellentesque at rhoncus sem. Etiam suscipit finibus magna quis volutpat. 
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
